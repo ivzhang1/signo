d0f2f0c0730b8f7f3158900dd4ef08ab3b844585
Ivan Zhang <izhang1@stuy.edu>
2018-11-08 12:14:07 -0500

    I've commited a sin

--- signo.c
+++ signo.c
@@ -2,9 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 static void sighandler(int signo){
   if(signo == SIGINT){
+    char * file = "text_file.txt";
+    int file_id = open(file, O_APPEND); //READ AND WRITE
+
+ int write_size = write(file_id, "Exit due to SIGINT\n", 64);
+    
     printf("\nExiting the Program\n");
     exit(0);
   }else if(signo == SIGUSR1){
