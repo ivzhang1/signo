8e4ccd03ab93cd5fb31b514d2effb6341d1b043d
ivzhang1 <izhang1@stuy.edu>
2018-11-08 20:29:48 -0500

    done

--- signo.c
+++ signo.c
@@ -6,10 +6,10 @@
 
 static void sighandler(int signo){
   if(signo == SIGINT){
-    char * file = "error.txt";
-    int file_id = open(file, O_CREAT|O_EXCL|O_WRONLY);
+    int file_id = open("error.txt", O_WRONLY | O_CREAT | O_APPEND, 0644);
 
-    int write_size = write(file_id, "Exit due to SIGINT\n", 32);
+    char str[] =  "Exit due to SIGINT\n";
+    int write_size = write(file_id, str, sizeof(str));
     close(file_id);
     printf("\nExiting the Program\n");
     exit(0);
@@ -21,8 +21,12 @@ static void sighandler(int signo){
 int main(){
   signal(SIGINT, sighandler);
   signal(SIGUSR1, sighandler);
+
   while(1){
     printf("PID: %d\n", getpid());
     sleep(1);
   }
+
+  return 0;
+  
 }
